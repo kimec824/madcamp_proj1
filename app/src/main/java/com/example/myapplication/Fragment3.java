d985e6028ad16e40c4b19bb2358e0e42b976cd5a
kimec824 <kimec@postech.ac.kr>
2021-01-06 00:05:08 +0900

    새 액티비티로 띄우기로 함

--- app/src/main/java/com/example/myapplication/Fragment3.java
+++ app/src/main/java/com/example/myapplication/Fragment3.java
@@ -33,139 +33,25 @@ import static androidx.core.content.ContextCompat.getSystemService;
 public class Fragment3 extends Fragment {
 
 
-    //boolean test=false;
-    /*public static Fragment3 newInstance() {
+    public static Fragment3 newInstance() {
 
         Bundle args = new Bundle();
 
         Fragment3 fragment = new Fragment3();
         fragment.setArguments(args);
         return fragment;
-    }*/
+    }
 
-
-    private Button mStartBtn, mStopBtn, mRecordBtn, mPauseBtn;
-    private TextView mTimeTextView, mRecordTextView;
-    private Thread timeThread = null;
-    private Boolean isRunning = true;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.alarm_execute, container, false);
+
         super.onCreate(savedInstanceState);
-        if (Build.VERSION.SDK_INT >= 21) {
-            getActivity().getWindow().setStatusBarColor(Color.parseColor("#4ea1d3"));
-        }
 
-        mStartBtn = (Button) view.findViewById(R.id.btn_start);
-        mStopBtn = (Button) view.findViewById(R.id.btn_stop);
-        mRecordBtn = (Button) view.findViewById(R.id.btn_record);
-        mPauseBtn = (Button) view.findViewById(R.id.btn_pause);
-        mTimeTextView = (TextView) view.findViewById(R.id.timeView);
-        mRecordTextView = (TextView) view.findViewById(R.id.recordView);
+        Intent intent = new Intent(getActivity(), Stopwatch.class);
+        startActivity(intent);
 
-        mStartBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                v.setVisibility(View.GONE);
-                mStopBtn.setVisibility(View.VISIBLE);
-                mRecordBtn.setVisibility(View.VISIBLE);
-                mPauseBtn.setVisibility(View.VISIBLE);
-
-                timeThread = new Thread(new timeThread());
-                timeThread.start();
-            }
-        });
-
-        mStopBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                v.setVisibility(View.GONE);
-                mRecordBtn.setVisibility(View.GONE);
-                mStartBtn.setVisibility(View.VISIBLE);
-                mPauseBtn.setVisibility(View.GONE);
-                mRecordTextView.setText("");
-                timeThread.interrupt();
-            }
-        });
-
-        mRecordBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mRecordTextView.setText(mRecordTextView.getText() + mTimeTextView.getText().toString() + "\n");
-            }
-        });
-
-        mPauseBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                isRunning = !isRunning;
-                if (isRunning) {
-                    mPauseBtn.setText("일시정지");
-                } else {
-                    mPauseBtn.setText("시작");
-                }
-            }
-        });
-        return view;
+        return null;
     }
 
-    @Override
-    public void onDestroyView(){
-        //test=true;
-        mTimeTextView=null;
-        mRecordTextView=null;
-        super.onDestroyView();
-    }
-
-
-
-    @SuppressLint("HandlerLeak")
-    Handler handler = new Handler() {
-
-        @Override
-            public void handleMessage(Message msg) {
-            int mSec = msg.arg1 % 100;
-            int sec = (msg.arg1 / 100) % 60;
-            int min = (msg.arg1 / 100) / 60;
-            int hour = (msg.arg1 / 100) / 360;
-            //1000이 1초 1000*60 은 1분 1000*60*10은 10분 1000*60*60은 한시간
-
-            @SuppressLint("DefaultLocale") String result = String.format("%02d:%02d:%02d:%02d", hour, min, sec, mSec);
-            if (result.equals("00:01:15:00")) {
-                Toast.makeText(getContext(), "1분 15초가 지났습니다.", Toast.LENGTH_SHORT).show();
-            }
-            mTimeTextView.setText(result);
-        }
-    };
-
-    public class timeThread implements Runnable {
-        @Override
-        public void run() {
-            int i = 0;
-
-
-            while (true) {
-                while (isRunning) { //일시정지를 누르면 멈춤
-                    Message msg = new Message();
-                    msg.arg1 = i++;
-                    handler.sendMessage(msg);
-
-                    try {
-                        Thread.sleep(10);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                        getActivity().runOnUiThread(new Runnable(){
-                            @Override
-                            public void run() {
-                                mTimeTextView.setText("");
-                                mTimeTextView.setText("00:00:00:00");
-                            }
-                        });
-                        return; // 인터럽트 받을 경우 return
-                    }
-                }
-            }
-        }
-    }
 }
